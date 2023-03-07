9a13ad2eaf616d5b9e6aa1c79d78b5f0b660cf09
Sk7805 <126848856+Sk7805@users.noreply.github.com>
2023-03-07 17:15:26 +0530

    Add files via upload

---
+++ requestform.php
@@ -0,0 +1,112 @@
+<?php
+
+require("class.phpmailer.php");
+require("class.pop3.php");
+
+
+if($_POST){
+	$cname=$_POST['name'];
+	$mobile=$_POST['mobile'];
+	$email=$_POST['email'];
+
+       
+//Third Party
+       $to=$email;
+       $mailer = new PHPMailer();
+     /*  $mailer->IsSMTP();
+       $mailer->SMTPSecure ='ssl';
+       $mailer->Host ='smtp.gmail.com';
+       $mailer->Port =465;
+       $mailer->SMTPAuth =TRUE;
+       $mailer->Username ='';  
+       $mailer->Password ='';  */
+       $mailer->From ='sofia.maddur@gmail.com';
+       $mailer->FromName ='sofia'; 
+       $mailer->IsHTML(true); 
+       $email_subject="Thank you for your interest";      
+       $email_body="
+
+       <p>Dear $name,<br/><br/>
+       Thanks for visiting and your valuable enquiry.<br> 
+       We will contact you soon!!
+       </p>
+
+
+       ";
+
+       $mailer->Body =$email_body;
+       $mailer->Subject =$email_subject;
+       $mailer->AddAddress($email);
+       $mailer->Send();
+
+ //Admin
+       
+       
+       $mailer1 = new PHPMailer();
+     /*  $mailer1->IsSMTP();
+       $mailer1->SMTPSecure ='ssl';
+       $mailer1->Host ='smtp.gmail.com';
+       $mailer1->Port =465;
+       $mailer1->SMTPAuth =TRUE;
+       $mailer1->Username ='';  
+       $mailer1->Password =''; 	*/			
+       $mailer1->From =$email;
+       $mailer1->FromName =$name; 
+       $mailer1->IsHTML(true);   
+
+       $email_subject="Book-a-venue form ";     
+       $email_body="
+       $name has enquired from the. 
+
+       Name : $cname
+       Mobile : $mobile
+       Email : $email
+
+
+
+       Thanks
+       
+       ";
+
+
+       $mailer1->Body =nl2br($email_body);
+       $mailer1->Subject =$email_subject;
+
+//TO
+$to_admin="sofia.maddur@gmail.com";
+       
+
+       $recipients_to1=explode(",",$to_admin);
+       foreach($recipients_to1 as $email1)
+       {
+       	$mailer1->AddAddress($email1);
+       }
+
+
+
+
+
+
+//$mailer1->AddCC($cc_address);
+// $mailer1->AddCC($cc_address1);
+
+//bcc
+
+       $bcc_address = "";
+       $mailer1->AddBCC($bcc_address);
+
+       if(!$mailer1->Send())
+       {
+       	echo "Message was not sent<br/ >";
+       	echo "Mailer Error: " .$mailer1->ErrorInfo;
+       }
+       else
+       { 
+
+
+
+       }
+
+   }
+
+   ?>
---
+++ validationscript.js
@@ -0,0 +1,83 @@
+/**CONTACT FORM START **/
+
+jQuery(document).ready(function(e) {
+    	
+	jQuery("#contactenquiryform").on('submit',function(e){
+				e.preventDefault();
+				var valid;	
+				valid = validateContact();
+				
+				if(valid) {
+					$("#btnsub").attr("disabled", true);
+					jQuery.ajax({
+					url: "https://dpstaging.in/Jobin/book-a-venue/mail/requestform.php",
+					type: "POST",
+					data:  new FormData(this),
+					contentType: false,
+					cache: false,
+					processData:false,
+					success: function(data){
+                        $("#mail-status").html(data);
+						document.getElementById("contactenquiryform").reset();
+						window.location.href="https://dpstaging.in/Jobin/book-a-venue/images/thankyou.jpg";
+						/*$('#loader-icon').hide();*/
+					},
+					error: function(){} 
+					});
+				}
+			
+		})
+});	
+function validateContact() {
+	var valid = true;	
+	jQuery("#contactenquiryform").css('border-color','black');
+	jQuery(".info").html('');
+	
+	if(!jQuery("#fname").val()) {
+		jQuery("#fname-info").html("Please Enter Name");
+		jQuery("#fname").css('border-color','red');
+		valid = false;
+	}
+	if(!jQuery("#fname").val().match(/^[a-zA-Z\s]+$/)) {
+		jQuery("#fname-info").html("Please Enter Valid Name");
+		jQuery("#fname").css('border-color','red');
+		valid = false;
+	}
+	if(!jQuery("#lname").val()) {
+		jQuery("#lname-info").html("Please Enter Name");
+		jQuery("#lname").css('border-color','red');
+		valid = false;
+	}
+	if(!jQuery("#lname").val().match(/^[a-zA-Z\s]+$/)) {
+		jQuery("#lname-info").html("Please Enter Valid Name");
+		jQuery("#lname").css('border-color','red');
+		valid = false;
+	}
+	
+	if(!jQuery("#cemail").val()) {
+		jQuery("#Email-info").html("Please Enter Email");
+		jQuery("#cemail").css('border-color','red');
+		valid = false;
+	}
+	if(!jQuery("#cemail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+		jQuery("#Email-info").html("Please Enter Valid Email");
+		jQuery("#cemail").css('border-color','red');
+		valid = false;
+	}
+	
+	
+	if(!jQuery("#cmessage").val().match(/^[a-zA-Z\s]+$/)) {
+		jQuery("#cMessage-info").html("Please Enter Characters");
+		jQuery("#cmessage").css('border-color','red');
+		valid = false;
+	}
+	
+	return valid;
+}
+$(function(){
+  $('#btnsub').attr('disabled', false);
+});
+
+
+
+
